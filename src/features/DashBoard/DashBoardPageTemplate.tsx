30b8f05d1504dd96867cdb4070bc967b46847221
keisuke071411 <i.love.football0714@gmail.com>
2023-02-18 16:19:12 +0900

    Add: サブスク一覧の描画

--- src/features/DashBoard/DashBoardPageTemplate.tsx
+++ src/features/DashBoard/DashBoardPageTemplate.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { useRouter } from "next/router";
 import { css } from "@emotion/react";
-import { Table, Loading } from "@nextui-org/react";
+import { Table, Loading, Button } from "@nextui-org/react";
 import { useRecoilValue } from "recoil";
 import { authState } from "~/store/auth";
 import { Container } from "~/components/layout/Container";
@@ -11,12 +11,14 @@ import { PAYMENT_STATUS } from "./constants";
 import { convertToMoney } from "~/utils/convertToMoney";
 import { formatUnixTimeDate } from "~/utils/formatUnixTimeDate";
 import { colors } from "styles/themes";
+import { stripeApi } from "~/context/ApiContext";
 
 export const DashBoardPageTemplate = ({
-  paymentList
+  paymentList,
+  subscriptionList
 }: DashBoardPageProps): JSX.Element => {
   const { currentUser, isLoading } = useRecoilValue(authState);
-  const { push } = useRouter();
+  const { push, reload } = useRouter();
 
   if (isLoading) return <Loading size="xl" />;
 
@@ -24,6 +26,16 @@ export const DashBoardPageTemplate = ({
     push("/");
     return <Fragment />;
   }
+
+  const handleClick = async (stripeCustomerId: string) => {
+    try {
+      await stripeApi.cancelSubscription(stripeCustomerId);
+
+      reload();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <main css={main}>
@@ -40,33 +52,84 @@ export const DashBoardPageTemplate = ({
             </p>
           </FlexContainer>
         </FlexContainer>
-        <p css={label}>注文履歴</p>
-        <Table
-          aria-label="Example static collection table"
-          css={{
-            height: "auto",
-            fontSize: "1.4rem"
-          }}
-        >
-          <Table.Header>
-            <Table.Column>金額</Table.Column>
-            <Table.Column>ステータス</Table.Column>
-            <Table.Column>作成日</Table.Column>
-          </Table.Header>
-          <Table.Body>
-            {paymentList.map((payment) => (
-              <Table.Row key={payment.id}>
-                <Table.Cell>{convertToMoney(payment.amount_total)}</Table.Cell>
-                <Table.Cell>
-                  {PAYMENT_STATUS[payment.payment_status]}
-                </Table.Cell>
-                <Table.Cell>
-                  {formatUnixTimeDate(payment.created, "jp", true)}
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
+        <div>
+          <p css={label}>サブスク一覧</p>
+          <Table
+            aria-label="Example static collection table"
+            css={{
+              height: "auto",
+              fontSize: "1.4rem"
+            }}
+          >
+            <Table.Header>
+              <Table.Column>金額</Table.Column>
+              <Table.Column>ステータス</Table.Column>
+              <Table.Column>契約日</Table.Column>
+              <Table.Column>契約更新日</Table.Column>
+              <Table.Column>cancel</Table.Column>
+            </Table.Header>
+            <Table.Body>
+              {subscriptionList.map((subscription) => (
+                <Table.Row key={subscription.id}>
+                  <Table.Cell>
+                    {convertToMoney(
+                      subscription.items.data[0].price.unit_amount
+                    )}
+                  </Table.Cell>
+                  <Table.Cell>{subscription.status}</Table.Cell>
+                  <Table.Cell>
+                    {formatUnixTimeDate(subscription.start_date, "jp")}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {formatUnixTimeDate(subscription.current_period_end, "jp")}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {subscription.status === "active" && (
+                      <Button
+                        auto
+                        color="error"
+                        onClick={() => handleClick(subscription.id)}
+                      >
+                        解約する
+                      </Button>
+                    )}
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+        <div style={{ marginTop: 40 }}>
+          <p css={label}>注文履歴</p>
+          <Table
+            aria-label="Example static collection table"
+            css={{
+              height: "auto",
+              fontSize: "1.4rem"
+            }}
+          >
+            <Table.Header>
+              <Table.Column>金額</Table.Column>
+              <Table.Column>ステータス</Table.Column>
+              <Table.Column>作成日</Table.Column>
+            </Table.Header>
+            <Table.Body>
+              {paymentList.map((payment) => (
+                <Table.Row key={payment.id}>
+                  <Table.Cell>
+                    {convertToMoney(payment.amount_total)}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {PAYMENT_STATUS[payment.payment_status]}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {formatUnixTimeDate(payment.created, "jp", true)}
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
       </Container>
     </main>
   );
